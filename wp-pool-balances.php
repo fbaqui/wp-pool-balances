00e56e68c27cc51934cfa8eef4920ac131d9102f
Felipe Baqui <felipe@eucontrolo.com.br>
2025-02-26 10:33:27 -0300

    Create wp-pool-balances.php

---
+++ wp-pool-balances.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WordPress Pool Balances Plugin
+ * Description: Plugin para exibir os saldos das pools de liquidez na Meteora.
+ * Version: 1.0.0
+ * Author: Felipe Baqui
+ * Author URI: https://github.com/fbaqui
+ * License: GPL v2 ou posterior
+ * Text Domain:pool-balances
+ */
